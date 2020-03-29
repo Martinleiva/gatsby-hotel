fa4ab6d9de122c4b0337492ea8e0a4dcbd49b4be
Martin <martin_leiva13@hotmail.com>
2020-03-29 00:49:28 -0300

    Proyecto completado.

---
+++ src/pages/nosotros.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+import ContenidoNosotros from '../components/contenidonosotros';
+
+const nosotros = () => {
+    return (
+        <Layout>
+            <ContenidoNosotros />
+        </Layout>
+    );
+};
+
+export default nosotros;
